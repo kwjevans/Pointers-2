1e7dc34767308c31da3192ea95caaafcb786e051
Kris Evans <kwjevans@gmail.com>
2012-08-13 16:41:26 +0100

    Initial Commit

---
+++ Pointers-2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Pointers-2
+//
+//  Created by Kris Evans on 13/08/2012.
+//  Copyright (c) 2012 Kris Evans. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
